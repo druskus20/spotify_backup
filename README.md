5eb103e1bd07b94568c0586cfe993fe551983eb3
druskus <druskus20@gmail.com>
2018-11-10 01:11:38 +0100

    Added the base project, created .gitignore, README.md and requirements.txt

---
+++ README.md
@@ -0,0 +1,25 @@
+# Spotify Playlist Exporter
+
+This is a basic program written in Python (version 3.6) to generate a json file with your playlists and songs data.
+
+It uses the Spotipy API and therefore the Spotify Web API
+https://github.com/plamere/spotipy
+
+##Setup:
+ 
+You need Python 3.X to run this program.
+Install the required packages with: 
+    $ pip install -r requirements.txt
+
+Create a file "credentials.txt" in the project root repository with:
+    CLIENT_KEY
+    CLIENT_SECRET
+
+Where CLIENT_KEY and CLIENT_SECRET are your user keys. You can find those key at: https://developer.spotify.com/dashboard/
+
+(those keys are yet not encoded, im planning on doing so in the near future)
+
+##Execution:
+
+Run "$ python app.py"
+
---
+++ app.py
@@ -0,0 +1,27 @@
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
+
+def run():
+    
+    keys = [line.rstrip('\n') for line in open('credentials.txt')]
+    print(keys)
+    os.environ['SPOTIPY_CLIENT_ID'] = keys[0]
+    os.environ['SPOTIPY_CLIENT_SECRET'] = keys[1]
+    #os.environ['SPOTIPY_REDIRECT_URI'] = "http://127.0.0.1:8080/callback"
+
+
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+    playlists = sp.user_playlists('druskus20')
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+if __name__ == "__main__":
+    run()
