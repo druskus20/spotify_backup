e69ecc390001082aa0bcbf5c1f14cf952a08d4a0
druskus <druskus20@gmail.com>
2018-11-10 02:54:57 +0100

    added functionality to print tracks

--- app.py
+++ app.py
@@ -9,19 +9,75 @@ def run():
     os.environ['SPOTIPY_CLIENT_ID'] = keys[0]
     os.environ['SPOTIPY_CLIENT_SECRET'] = keys[1]
     #os.environ['SPOTIPY_REDIRECT_URI'] = "http://127.0.0.1:8080/callback"
-
+    username = keys[2]
 
     client_credentials_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-    playlists = sp.user_playlists(keys[2])
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-        if playlists['next']:
-            playlists = sp.next(playlists)
-        else:
-            playlists = None
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == username:
+            print ()
+            print (playlist['name'])
+            print ('  total tracks', playlist['tracks']['total'])
+            results = sp.user_playlist(username, playlist['id'],
+                fields="tracks,next")
+            tracks = results['tracks']
+            show_tracks(tracks)
+            while tracks['next']:
+                tracks = sp.next(tracks)
+                show_tracks(tracks)
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print ("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+
 if __name__ == "__main__":
     run()
+
+    """# shows a user's playlists (need to be authenticated via oauth)
+
+import sys
+import spotipy
+import spotipy.util as util
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print "   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name'])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print "Whoops, need your username!"
+        print "usage: python user_playlists.py [username]"
+        sys.exit()
+
+    token = util.prompt_for_user_token(username)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print
+                print playlist['name']
+                print '  total tracks', playlist['tracks']['total']
+                results = sp.user_playlist(username, playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    show_tracks(tracks)
+    else:
+        print "Can't get token for", username
+        """
+        
